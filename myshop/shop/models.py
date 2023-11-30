4198c7c27835ce96aba0eeae5e6f3069fc7a3f1d
Glazapolzet <VypZlol@yandex.ru>
2023-11-30 22:32:38 +0300

    clean and smooth e-commerce

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,19 +1,12 @@
 from django.db import models
-from parler.models import TranslatableModel, TranslatedFields
 
 
-class Category(TranslatableModel):
+class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
 
-    # translations = TranslatedFields(
-    #     name=models.CharField(max_length=200, db_index=True),
-    #     slug=models.SlugField(max_length=200, db_index=True, unique=True),
-    # )
-
     def __str__(self):
-        # return self.name
-        return "Category"
+        return self.name
     
     class Meta:
         ordering = ('name',)
@@ -24,18 +17,13 @@ class Category(TranslatableModel):
         return f"/shop/{self.slug}"
 
 
-class Product(TranslatableModel):
+class Product(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     description = models.TextField(blank=True)
 
-    # translations = TranslatedFields(
-    #     name=models.CharField(max_length=200, db_index=True),
-    #     slug=models.SlugField(max_length=200, db_index=True),
-    #     description=models.TextField(blank=True)
-    # )
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', null=True, blank=True, verbose_name='Изображение')
+    image = models.ImageField(upload_to='products/%Y/%m/%d', null=True, blank=True, verbose_name='Image')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     available = models.BooleanField(default=True)
@@ -43,8 +31,7 @@ class Product(TranslatableModel):
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        # return self.name
-        return "Product"
+        return self.name
     
     class Meta:
         ordering = ('name',)
